a5f62549ea94bd0dfaae97e7931a85d6aabeff69
Qing Miao <digisanQM@outlook.com>
2021-03-26 10:09:26 +1100

    + progress bar

--- go.mod
+++ go.mod
@@ -5,4 +5,10 @@ go 1.16
 require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/cdutwhu/csv-tool v0.0.6
+	github.com/cdutwhu/debog v0.2.12 // indirect
+	github.com/digisan/gotk v0.0.3
+	github.com/gosuri/uilive v0.0.4 // indirect
+	github.com/gosuri/uiprogress v0.0.1
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	golang.org/x/sys v0.0.0-20210324051608-47abb6519492 // indirect
 )
--- split.go
+++ split.go
@@ -1,24 +1,50 @@
 package splitter
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+	"sync/atomic"
 
 	csvtool "github.com/cdutwhu/csv-tool"
+	gio "github.com/digisan/gotk/io"
+	"github.com/gosuri/uiprogress"
 	"github.com/nsip/dev-nrt-splitter/config"
 )
+
+var (
+	uip      *uiprogress.Progress
+	bar      *uiprogress.Bar
+	procsize uint64
+	progbar  = true
+)
+
+func EnableProgBar(enable bool) {
+	progbar = enable
+}
 
 // NrtSplit :
 func NrtSplit(configurations ...string) error {
 	defer os.RemoveAll("./tempcsv")
 
 	cfg := config.GetConfig(configurations...)
-	fmt.Println(cfg.InFolder)
+	// fmt.Println(cfg.InFolder)
 	inDirAbs, err := filepath.Abs(cfg.InFolder)
 	if err != nil {
 		return err
+	}
+
+	// -- progress bar 1 -- //
+	if progbar {
+		uip = uiprogress.New()
+		defer uip.Stop()
+		uip.Start()
+		cnt, _, err := gio.FileDirCount(inDirAbs, true)
+		if err != nil {
+			return err
+		}
+		bar = uip.AddBar(cnt)
+		bar.AppendCompleted().PrependElapsed()
 	}
 
 	err = filepath.Walk(cfg.InFolder, func(path string, info os.FileInfo, err error) error {
@@ -49,13 +75,20 @@ func NrtSplit(configurations ...string) error {
 		}
 
 		if cfg.Trim.Enabled {
-			fmt.Printf("Trim Processing...: %v\n", fPath)
+			// fmt.Printf("Trim Processing...: %v\n", fPath)
 			csvtool.Query(fPath, false, cfg.Trim.Columns, '&', nil, cfg.Trim.OutFolder+fName, nil)
 		}
 
 		if cfg.Splitting.Enabled {
-			fmt.Printf("Split Processing...: %v\n", fPath)
+			// fmt.Printf("Split Processing...: %v\n", fPath)
 			csvtool.Split(fPath, cfg.Splitting.OutFolder, false, cfg.Splitting.Schema...)
+		}
+
+		// -- progress bar 2 -- //
+		if progbar {
+			atomic.AddUint64(&procsize, 1)
+			bar.Set(int(procsize))
+			bar.Incr()
 		}
 
 		return nil
